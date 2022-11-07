3b2a1fd2b3722031f727258877a6d985363667c2
Kapsaloco <kapsalakosg@gmail.com>
2022-11-07 12:36:40 +0100

    Create data.sql

---
+++ QuestionnaireAssessment/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into Users (email, password) values ('George', 'Kapsalakos'); 
+insert into Users (email, password) values ('Josh', 'Baba'); 
+insert into Users (email, password) values ('Christian', 'Michael');
+insert into Question (q_id, question) values (1, 'How much do you lift?');
+insert into Question (q_id, question) values (2, 'How many did you pull last night?');
+insert into Answer (a_id, answers, q_id) values (1, 'Little', 1);
+insert into Answer (a_id, answers, q_id) values (2, 'Much AF', 1);
+insert into Answer (a_id, answers, q_id) values (3, '0', 2);
+insert into Answer (a_id, answers, q_id) values (4, 'Ask ur', 2);
