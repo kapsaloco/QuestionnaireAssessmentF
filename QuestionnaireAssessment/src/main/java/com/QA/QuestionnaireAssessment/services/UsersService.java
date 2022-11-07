d1a97890e49606156bfd6610fc249bc37d9b72e7
Cris-Sand <cristian.mihai@solera.com>
2022-11-07 12:39:48 +0100

    Initialized UsersInterface and UsersService

---
+++ QuestionnaireAssessment/src/main/java/com/QA/QuestionnaireAssessment/services/UsersService.java
@@ -0,0 +1,11 @@
+package com.QA.QuestionnaireAssessment.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.QA.QuestionnaireAssessment.repositories.IAnswersRepository;
+
+@Service
+public class UsersService {
+
+}
