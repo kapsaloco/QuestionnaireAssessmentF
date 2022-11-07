97d958a2a23fe3456086900546414819d736d0c1
Kapsaloco <kapsalakosg@gmail.com>
2022-11-07 09:58:14 +0100

    Initial Commit

---
+++ QuestionnaireAssessment/src/main/java/com/QA/QuestionnaireAssessment/QuestionnaireAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.QA.QuestionnaireAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionnaireAssessmentApplication.class, args);
+	}
+
+}
---
+++ QuestionnaireAssessment/src/test/java/com/QA/QuestionnaireAssessment/QuestionnaireAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QA.QuestionnaireAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
