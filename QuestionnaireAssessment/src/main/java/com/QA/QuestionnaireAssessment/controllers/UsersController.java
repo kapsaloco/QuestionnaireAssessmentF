55b48ae285a40c6b0b7eee492e1e91445c50f514
Cris-Sand <cristian.mihai@solera.com>
2022-11-07 16:43:38 +0100

    Added CrossOrigin to UserController

--- QuestionnaireAssessment/src/main/java/com/QA/QuestionnaireAssessment/controllers/UsersController.java
+++ QuestionnaireAssessment/src/main/java/com/QA/QuestionnaireAssessment/controllers/UsersController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +26,7 @@ import com.QA.QuestionnaireAssessment.models.Users;
 import com.QA.QuestionnaireAssessment.services.UsersService;
 
 
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class UsersController {
