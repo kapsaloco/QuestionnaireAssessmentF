fed3ea1564e6e0be670f5da5c78bd3027ca6e145
Babajide Oke <josua.oke@solera.com>
2022-11-07 12:08:13 +0100

    created Answer entity

--- QuestionnaireAssessment/src/main/java/com/QA/QuestionnaireAssessment/models/Answer.java
+++ QuestionnaireAssessment/src/main/java/com/QA/QuestionnaireAssessment/models/Answer.java
@@ -1,5 +1,60 @@
 package com.QA.QuestionnaireAssessment.models;
 
-public class Answer {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Answer {
+	@Id
+	@SequenceGenerator(
+			name="answer_sequence",
+			sequenceName = "answer_sequence"
+			)
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "answer_sequence"
+			)
+	private long aId;
+	
+	@ManyToOne
+	@JoinColumn(name="qId")
+	Question question;
+	@Column
+	String answers;
+
+
+	private Answer(Question question, String answers) {
+		super();
+		this.question = question;
+		this.answers = answers;
+	}
+
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public String getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(String answers) {
+		this.answers = answers;
+	}
+	
 }
---
+++ QuestionnaireAssessment/src/main/java/com/QA/QuestionnaireAssessment/repositories/IAnswersRepository.java
@@ -0,0 +1,5 @@
+package com.QA.QuestionnaireAssessment.repositories;
+
+public interface IAnswersRepository {
+
+}
